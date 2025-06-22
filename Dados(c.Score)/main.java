5dcf8bb4515576bc740695a262667213dc54bfa0
Antônio Eugênio <162815265+EugenioJuni0r@users.noreply.github.com>
2025-06-22 01:42:39 -0300

    Update main.java

---
+++ Dados(c.Score)/main.java
@@ -0,0 +1,11 @@
+import JogoDosDados.*;
+public class main{
+    public static void main(String[] args) throws Exception{
+        Jogo run = new Jogo();
+        run.inserirJogadores();
+        run.inserirApostas();
+        run.lancarDados();
+        run.mostrarDados();
+        run.mostrarVencedores();
+    }
+}
