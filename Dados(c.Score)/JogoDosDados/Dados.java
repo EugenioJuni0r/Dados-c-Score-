a724eaa62d2e753aafef85329fdb330442e005b1
Antônio Eugênio <162815265+EugenioJuni0r@users.noreply.github.com>
2025-06-22 01:39:23 -0300

    Update Dados.java

--- Dados(c.Score)/JogoDosDados/Dados.java
+++ Dados(c.Score)/JogoDosDados/Dados.java
@@ -1,1 +1,13 @@
 package JogoDosDados;
+
+public class Dados{
+
+    private int valorFace;
+
+    public int getValorFace(){
+        return valorFace;
+    }
+    public void setValorFace(){
+        valorFace = (int)(1+Math.random()*6);
+    }
+}
