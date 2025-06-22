e6c15d2ea08027302a8981f92c5042c609840766
Antônio Eugênio <162815265+EugenioJuni0r@users.noreply.github.com>
2025-06-22 00:28:14 -0300

    Add files via upload

---
+++ Dados(c.Score)/JogoDosDados/Jogador.java
@@ -0,0 +1,17 @@
+package JogoDosDados;
+public class Jogador{
+    String name;
+    int valorDaAposta;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getValorDaAposta(){
+        return valorDaAposta;
+    }
+    public void setValorDaAposta(int valorDaAposta){
+        this.valorDaAposta = valorDaAposta;
+    }
+}
