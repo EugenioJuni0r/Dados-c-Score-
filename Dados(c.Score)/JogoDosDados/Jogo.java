bb78146f782a9df9d0d7738a7f00eaf07bfe635b
Antônio Eugênio <162815265+EugenioJuni0r@users.noreply.github.com>
2025-06-27 12:49:02 -0300

    Update Jogo.java

--- Dados(c.Score)/JogoDosDados/Jogo.java
+++ Dados(c.Score)/JogoDosDados/Jogo.java
@@ -8,11 +8,12 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Jogo{
-
     Scanner ler = new Scanner(System.in);
-    ArrayList<Jogador> jogadores = new ArrayList<>();
-    Dados dado1 = new Dados();
-    Dados dado2 = new Dados();
+    String vencedor = null;
+    String path = "C:\\Users\\SUporte\\Documents\\POO - Programação Orientada a Objeto\\sim\\ranking.csv";
+    private ArrayList<Jogador> jogadores = new ArrayList<>();
+    private Dados dado1 = new Dados();
+    private Dados dado2 = new Dados();
 
     public void inserirJogadores() {
         boolean continuar = false;
@@ -21,8 +22,6 @@ public class Jogo{
             Jogador jogador = new Jogador();
             System.out.print("Digite o nome do jogador: ");
             jogador.setName(ler.nextLine());
-            System.out.println("Digite o ID desse usuario: ");
-            jogador.setId(ler.nextInt());
             jogadores.add(jogador);
             System.out.print("Deseja adicionar mais alguém? (1-SIM): ");
             op = ler.nextInt();
@@ -35,12 +34,15 @@ public class Jogo{
         }while(continuar);
     }
     public void inserirApostas(){
-        for (Jogador jogador : jogadores) {
-            System.out.print("Digite a aposta de " + jogador.getName() + ": ");
-            int aposta = ler.nextInt();
-            jogador.setValorDaAposta(aposta);
+        for (Jogador jogador : jogadores){
+            System.out.print("Digite a aposta de " + jogador.getName() + "(2 a 12): ");
+            int aposta;
+            do{
+                aposta = ler.nextInt();
+                jogador.setValorDaAposta(aposta);
+            }while(aposta > 12 || 2 > aposta);
+            ler.nextLine();
         }
-        ler.nextLine();
     }
     public void lancarDados(){
         dado1.setValorFace();
@@ -60,69 +62,12 @@ public class Jogo{
                 }
                 jogador.adicionarVitoria();
                 System.out.println("Nome " + ++contWin + "° vencedor: " + jogador.getName());
+                vencedor = "Nome: " + jogador.getName() + ";Vitorias: " + jogador.getVitorias() + "\n";
+                MyFileHandle.write(path, vencedor ,true);
             }
         }
         if(contWin == 0){
             System.out.println("Não houveram ganhadores...");
         }
-    }
-    public void mostrarRanking(){
-        
-        int posicao = 1;
-
-        Collections.sort(jogadores);
-        System.out.println("Ranking de Vitorias!: ");
-        for(Jogador jogador: jogadores){
-            System.out.println( posicao++ + "° lugar: " + jogador.getName() + "-" + jogador.getVitorias() + "vitoria(s).");
-        }
-    }
-    public void salvarRankingCSV(String nomeArquivo) throws IOException {
-        PrintWriter writer = new PrintWriter(new FileWriter(nomeArquivo));
-        writer.println("Nome,Vitorias");
-        for (Jogador jogador : jogadores){
-            writer.println(jogador.getName() + "," + jogador.getVitorias());
-        }
-        writer.close();
-        System.out.println("Ranking salvo no arquivo: " + nomeArquivo);
-    }
-}
-    public void mostrarDados(){
-        System.out.println("Dado 1: " + dado1.getValorFace());
-        System.out.println("Dado 2: " + dado2.getValorFace());
-        System.out.println("E a soma dos dados deu: " + (dado1.getValorFace() + dado2.getValorFace()));
-    }
-    public void mostrarVencedores(){
-        int contWin = 0;
-        for(Jogador jogador: jogadores){
-            if(jogador.getValorDaAposta() == 3){
-                if(contWin == 0){
-                    System.out.println("Houveram vencedores no jogo!");
-                }
-                jogador.adicionarVitoria();
-                System.out.println("Nome " + ++contWin + "° vencedor: " + jogador.getName());
-            }
-        }
-        if(contWin == 0){
-            System.out.println("Não houveram ganhadores...");
-        }
-    }
-    public void mostrarRanking(){
-        
-        int posicao = 1;
-
-        Collections.sort(jogadores);
-        System.out.println("Ranking de Vitorias!: ");
-        for(Jogador jogador: jogadores){
-            System.out.println( posicao++ + "° lugar: " + jogador.getName() + "-" + jogador.getVitorias() + "vitoria(s).");
-        }
-    }
-    public void salvarRankingCSV(String nomeArquivo) throws IOException {
-        PrintWriter writer = new PrintWriter(new FileWriter(nomeArquivo));
-        writer.println("Nome,Vitorias");
-        for (Jogador jogador : jogadores){
-            writer.println(jogador.getName() + "," + jogador.getVitorias());
-        }
-        writer.close();
-        System.out.println("Ranking salvo no arquivo: " + nomeArquivo);
     }
 }
