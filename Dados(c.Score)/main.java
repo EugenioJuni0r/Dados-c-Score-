cf0270d632c661849185e23647e32c9669480b39
Antônio Eugênio <162815265+EugenioJuni0r@users.noreply.github.com>
2025-06-27 12:51:02 -0300

    Update main.java

--- Dados(c.Score)/main.java
+++ Dados(c.Score)/main.java
@@ -3,21 +3,11 @@ import java.io.IOException;
 
 public class main{
     public static void main(String[] args) throws Exception{
-        
         Jogo run = new Jogo();
         run.inserirJogadores();
         run.inserirApostas();
         run.lancarDados();
         run.mostrarDados();
         run.mostrarVencedores();
-        
-        try{
-            run.salvarRankingCSV("ranking.csv");
-            System.out.println("Ranking salvo em 'ranking.csv'");
-        }catch(IOException e){
-            System.out.println("Erro ao salvar o ranking: " + e.getMessage());
-        }
-        System.out.println("Ranking Geral!:");
-        run.mostrarRanking();
     }
 }
